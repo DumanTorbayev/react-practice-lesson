6845a4a6f503b0ff21afec054a8b3af57f34461b
DumanTorbayev <dumanchikt@gmail.com>
2020-09-04 23:11:32 +0600

    fix for browser Firefox

--- src/components/SortPopup.jsx
+++ src/components/SortPopup.jsx
@@ -16,7 +16,8 @@ const SortPopup = ({items, handleSortByType, activeSortType}) => {
     };
 
     const handleOutsideClick = (e) => {
-        if (!e.path.includes(sortRef.current)) {
+        const path = e.path || (e.composedPath && e.composedPath())
+        if (path.includes(sortRef.current)) {
             setVisiblePopup(false);
         }
     }
